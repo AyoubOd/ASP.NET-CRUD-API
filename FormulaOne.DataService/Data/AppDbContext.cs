3a8c6caa33cede9c758a1ede26b0498d65801352
AyoubOd <ayouboudada31@gmail.com>
2023-08-25 13:10:22 +0100

    Adding context class

---
+++ FormulaOne.DataService/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+using FormulaOne.Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+
+namespace FomulaOne.DataService.Data;
+
+public class AppDbContext : DbContext
+{
+    public virtual DbSet<Driver> Drivers { get; set; }
+    public virtual DbSet<Achievement> Achievements { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Driver>(entity =>
+            {
+                entity.HasMany<Achievement>()
+                    .WithOne(e => e.Driver)
+                    .HasForeignKey(e => e.DriverId)
+                    .OnDelete(DeleteBehavior.SetNull);
+            }
+        );
+    }
+}
